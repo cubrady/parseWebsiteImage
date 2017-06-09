36387dc71a45743932cb5aab720e2ab0c77ddd33
Brad Chang <cutebrady@gmail.com>
2017-06-09 11:25:13 +0800

    Add more check functions

--- parsePic.py
+++ parsePic.py
@@ -3,6 +3,8 @@
 import os, time
 import re, sys
 import hashlib
+import imghdr
+from shutil import copyfile
 
 from WebUtility import getWebContent
 
@@ -23,14 +25,6 @@ def parseWebImages(targetUrl, folder, lstMd5Sum):
     for url in urlList:
         if ".jpg" in url and "http://" in url:
             os.popen('wget -nv "%s" -P %s' % (url, folder))
-            # imgPath = wget.download(url, out = folder)
-            # md5sum = md5(imgPath)
-            # if md5sum not in lstMd5Sum:
-            #     lstMd5Sum.append(md5sum)
-            # else:
-            #     os.remove(imgPath)
-            #     print "Remove %s" % imgPath
-            #time.sleep(0.2)
             print "\n"
         if i % 50 == 0 or i > len(urlList) - 3:
             print "[%.2f%%] %d / %d " % (i / float(len(urlList)) * 100, i, len(urlList))
@@ -68,7 +62,7 @@ def getAllImages(folderPath, count, lstFolder):
 
     lstPath = os.listdir(folderPath)
     for path in lstPath:
-        if os.path.isfile(path) and path.endswith(".jpg"):
+        if os.path.isfile(path) and (path.endswith(".jpg") or path.endswith(".jpeg")):
             count += 1
         else:
             count += getAllImages(os.path.join(folderPath, path), 0, lstFolder)
@@ -80,6 +74,51 @@ def showFolderImgCount(lstFolder):
     for folderPath, count in lstFolder:
        print "%s : %d" % (folderPath, count)
 
+def copyFolderFiles(srcPath, tarFolder):
+    copiedFiles = 0
+    files = os.listdir(srcPath)
+    for f in files:
+        srcFilePath = os.path.join(srcPath, f)
+        srcFileMd5 = md5(srcFilePath)
+        tarFilePath = os.path.join(tarFolder, srcFileMd5 + ".jpg")
+        copyfile(srcFilePath, tarFilePath)
+        copiedFiles += 1
+    return copiedFiles, len(files)
+
+def mergeFolders(srcFolder, count, tarFolder):
+    root_path = "/home/brad_chang/Share/dataset/xxx_images"
+    totalSrcFile = 0
+    copiedFiles = 0
+    for i in xrange(1, count):
+        src = srcFolder % i
+        srcPath = os.path.join(root_path, src)
+        copied, total = copyFolderFiles(srcPath, os.path.join(root_path, tarFolder))
+        '''
+        files = os.listdir(srcPath)
+        totalSrcFile += len(files)
+        for f in files:
+            srcFilePath = os.path.join(root_path, srcPath, f)
+            srcFileMd5 = md5(srcFilePath)
+            tarFilePath = os.path.join(root_path, tarFolder, srcFileMd5 + ".jpg")
+            copyfile(srcFilePath, tarFilePath)
+            copiedFiles += 1
+        '''
+        totalSrcFile += total
+        copiedFiles += copied
+        print "%s folder complete " % src
+
+    print "Copied %d / %d " % (copiedFiles, totalSrcFile)
+
+def checkValidImage(srcFolder, format = "jpeg"):
+    if os.path.isfile(srcFolder):
+        if imghdr.what(srcFolder) != format:
+            print "Invlaid format:%s, Remove : %s" % (imghdr.what(srcFolder), srcFolder)
+            os.remove(srcFolder)
+    else:
+        lstPath = os.listdir(srcFolder)
+        for path in lstPath:
+            checkValidImage(os.path.join(srcFolder, path), format)
+
 if __name__ == '__main__':
     t = time.time()
 
@@ -89,10 +128,11 @@ if __name__ == '__main__':
         #OUTPUT + "/xnxx1" : "http://www.xnxx.com/home/1",
         }
 
-    for i in xrange(0, 109):
+    for i in xrange(0, 1):
         # http://multi.xnxx.com/p-2
         #http://www.xnxx.com/best/108/
         dicWebsites[OUTPUT + "/xnxx_best_v%d" % i] = "http://www.xnxx.com/best/%d/" % i
+        pass
 
     for folder, url in dicWebsites.iteritems():
         checkFolder(folder)
@@ -104,4 +144,4 @@ if __name__ == '__main__':
     lstFolder = []
     print "Now we have %d images !!" % getAllImages(OUTPUT, 0, lstFolder)
 
-    #showFolderImgCount(lstFolder)
+    showFolderImgCount(lstFolder)
